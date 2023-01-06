67d845cccdf1215b184b8c20d3fab4215a5b2d1c
Naimhz <sekki691@gmail.com>
2023-01-06 21:12:57 +0100

    debut login

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,19 +1,39 @@
 function login(){
-    let id = document.getElementsByName("username").value;
-    let pwd = document.getElementsByName("password").value;
-    if(checkLogs(id,pwd)){
+    let sendButton = document.getElementById("login__submit")
+    sendButton.addEventListener("click", function(e){ 
+        e.preventDefault();
+        let id = document.getElementsByName("username").value;
+        let pwd = document.getElementsByName("password").value;
+        if(checkLogs(id,pwd)){
+        console.log("ok");
 
-    }
+        }else{
+            console.log("ntm");
+        }
+    })
+  
 }
 
-function checkLogs(id,pwd){
+function checkLogs(a,b){
     fetch('http://localhost:5678/api/users/login', {
   method: 'POST',
   headers: {
     'Accept': 'application/json', 
     'Content-Type': 'application/json'
   },
-  body: JSON.stringify({email:id, password:pwd})
-}).then(res => res.json())
-  .then(res => console.log(res));
+  body: JSON.stringify({email: a, password:b})
+}).then(function(res){ 
+        if(res.ok) {
+                return(true); 
+                }else{
+                    throw new error();
+                }}
+) .catch(function(err)  {console.log(err)
+                return (false);
+});
 }
+
+login();
+            
+        
+ 
